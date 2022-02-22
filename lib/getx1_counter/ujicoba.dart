acb7838d79c6106de63581c28f556271f2d26458
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-02-23 00:42:22 +0700

    testing for theme

---
+++ lib/getx1_counter/ujicoba.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UjiCOba extends StatelessWidget {
+  const UjiCOba({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
