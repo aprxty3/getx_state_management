00323fd124eae1bbd29b2eaa18df6e921ac7935f
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-02-23 00:26:16 +0700

    add change themedata

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_state_management/day1_counter/getx1.dart';
+
+import 'getx1_counter/controller/counter_controller.dart';
+import 'getx1_counter/getx1.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final counterC = Get.put(CounterController());
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GetX1(),
+    return Obx(
+      () => MaterialApp(
+        theme: counterC.isDark.value ? ThemeData.dark() : ThemeData.light(),
+        home: GetX1(),
+      ),
     );
   }
 }
