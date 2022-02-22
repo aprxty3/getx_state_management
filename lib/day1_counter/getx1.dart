9b14028a98e088769e429d908ae564961a59c592
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-02-23 00:08:48 +0700

    implement get x counter

--- lib/day1_counter/getx1.dart
+++ lib/day1_counter/getx1.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_state_management/day1_counter/controller/counter_controller.dart';
 
 class GetX1 extends StatelessWidget {
-  const GetX1({Key? key}) : super(key: key);
+  final counterC = Get.put(CounterController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+          child: Obx(
+        () => Text(
+          'Angka ${counterC.counter}',
+          style: TextStyle(
+            fontSize: 38,
+          ),
+        ),
+      )),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () => counterC.increment()),
     );
   }
 }
