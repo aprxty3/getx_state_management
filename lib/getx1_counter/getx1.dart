318d8af76e29ca16b300a723246ee43ab09ea7dc
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-02-23 00:42:45 +0700

    testing inc, dec, and change theme function

--- lib/getx1_counter/getx1.dart
+++ lib/getx1_counter/getx1.dart
@@ -1,25 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_state_management/getx1_counter/ujicoba.dart';
 import 'controller/counter_controller.dart';
 
 class GetX1 extends StatelessWidget {
-  final counterC = Get.put(CounterController());
+  final c = Get.find<CounterController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-          child: Obx(
-        () => Text(
-          'Angka ${counterC.counter}',
-          style: TextStyle(
-            fontSize: 38,
-          ),
+        child: Column(
+          children: [
+            Obx(
+              () => Text(
+                'Angka ${c.counter}',
+                style: TextStyle(
+                  fontSize: 38,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () => c.increment(),
+                  icon: Icon(
+                    Icons.plus_one,
+                    size: 40,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => c.decrement(),
+                  icon: Icon(
+                    Icons.minimize,
+                    size: 40,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            IconButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UjiCOba(),
+                ),
+              ),
+              icon: Icon(
+                Icons.swipe,
+                size: 30,
+              ),
+            ),
+          ],
         ),
-      )),
+      ),
       floatingActionButton:
-          FloatingActionButton(onPressed: () => counterC.increment()),
+          FloatingActionButton(onPressed: () => c.changeTheme()),
     );
   }
 }
