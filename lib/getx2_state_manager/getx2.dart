7f9f5cb15d6f98ab5292a3bef5bda6c296153baa
aprxty3 <ajiprxty3@gmail.com>
2022-02-24 02:43:20 +0700

    counter

--- lib/getx2_state_manager/getx2.dart
+++ lib/getx2_state_manager/getx2.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GetX2 extends StatelessWidget {
-  const GetX2({Key? key}) : super(key: key);
+  var count = 0.obs;
+  void add() {
+    count++;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Obx(
+          () => Text(
+            'Angka $count',
+            style: TextStyle(fontSize: 30),
+          ),
+          // Text(
+          //   'Nama Saya Aji',
+          //   style: TextStyle(fontSize: 30),
+          // ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => add(),
+      ),
     );
   }
 }
