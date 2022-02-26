50a3ed563da8cdca48983fd0688f9820fae28841
aprxty3 <ajiprxty3@gmail.com>
2022-02-27 04:10:54 +0700

    change name

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:getx_state_management/getx2_state_manager/getx2.dart';
 import 'package:getx_state_management/getx3_uniqueID/get3.dart';
 import 'package:getx_state_management/getx4_life_cycle/get4.dart';
 import 'package:getx_state_management/getx5_workers/get5.dart';
-import 'package:getx_state_management/getx6_navigation/get6.dart';
+import 'package:getx_state_management/getx6_navigation/get6_1.dart';
 
 import 'getx1_counter/controller/counter_controller.dart';
 import 'getx1_counter/getx1.dart';
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.light(),
-      home: Get6(),
+      home: Get6_1(),
     );
   }
 }
