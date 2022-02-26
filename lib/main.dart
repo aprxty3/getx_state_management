53b1b972f1d5d452c1f0006dca3197f08133152c
aprxty3 <ajiprxty3@gmail.com>
2022-02-27 01:01:48 +0700

    route to get5

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:getx_state_management/getx2_state_manager/getx2.dart';
 import 'package:getx_state_management/getx3_uniqueID/get3.dart';
 import 'package:getx_state_management/getx4_life_cycle/get4.dart';
+import 'package:getx_state_management/getx5_workers/get5.dart';
 
 import 'getx1_counter/controller/counter_controller.dart';
 import 'getx1_counter/getx1.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.light(),
-      home: Get4(),
+      home: Get5(),
     );
   }
 }
