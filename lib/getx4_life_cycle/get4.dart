ea6e401b2f1966373113cd4beba6414c7c2157c0
aprxty3 <ajiprxty3@gmail.com>
2022-02-26 23:44:25 +0700

    try to code TextController with GetX

--- lib/getx4_life_cycle/get4.dart
+++ lib/getx4_life_cycle/get4.dart
@@ -2,45 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Get4 extends StatelessWidget {
-  final countC = Get.put(CountController());
+  // final countC = Get.put(CountController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => OtherPage(),
+              ),
+            ),
+            icon: Icon(
+              Icons.chevron_right,
+            ),
+          ),
+        ],
+        title: Text('Home Page'),
+      ),
       body: Center(
-        child: CountWidget(),
+        child: Text('HOMEPAGE'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          countC.add();
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     countC.add();
+      //   },
+      // ),
     );
   }
 }
 
-class CountWidget extends StatelessWidget {
-  // final c = Get.find<CountController>();
+// class CountWidget extends StatelessWidget {
+//   // final c = Get.find<CountController>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetBuilder<CountController>(
+//       initState: (state) => print('initstate'),
+//       dispose: (state) => print('dispose'),
+//       didChangeDependencies: (_) => print('didChangeDependencies'),
+//       didUpdateWidget: (oldWidget, state) => print('didUpdateWidget'),
+//       builder: (c) => Center(
+//         child: Text('Angka ${c.count}'),
+//       ),
+//     );
+//   }
+// }
+
+class OtherPage extends StatelessWidget {
+  final textC = Get.put(TextControl());
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<CountController>(
-      initState: (state) => print('initstate'),
-      dispose: (state) => print('dispose'),
-      didChangeDependencies: (_) => print('didChangeDependencies'),
-      didUpdateWidget: (oldWidget, state) => print('didUpdateWidget'),
-      builder: (c) => Center(
-        child: Text('Angka ${c.count}'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Page'),
+      ),
+      body: Center(
+        child: TextField(
+          controller: textC.myC,
+        ),
       ),
     );
   }
 }
 
-class CountController extends GetxController {
-  var count = 0;
+// class CountController extends GetxController {
+//   var count = 0;
 
-  void add() {
-    count++;
-    update();
-  }
+//   void add() {
+//     count++;
+//     update();
+//   }
+// }
+
+class TextControl extends GetxController {
+  final myC = TextEditingController();
 }
