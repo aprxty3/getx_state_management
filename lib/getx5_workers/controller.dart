26dcd51bd140ca6da6a02b373f03c20f6b707844
aprxty3 <ajiprxty3@gmail.com>
2022-02-27 03:57:55 +0700

    inactive file code

--- lib/getx5_workers/controller.dart
+++ lib/getx5_workers/controller.dart
@@ -1,53 +1,53 @@
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
-class Controller extends GetxController {
-  var count = 0.obs;
-  var data = 0.obs;
+// class Controller extends GetxController {
+//   var count = 0.obs;
+//   var data = 0.obs;
 
-  void change() => count++;
+//   void change() => count++;
 
-  void reset() => count.value = 0;
+//   void reset() => count.value = 0;
 
-  @override
-  void onInit() {
-    //Kelima function dibawah atau disebut Workers, hanya dapat berjalan di onInit()
+//   @override
+//   void onInit() {
+//     //Kelima function dibawah atau disebut Workers, hanya dapat berjalan di onInit()
 
-    // TODO: Ever Berguna untuk melihat single data yg berjalan
-    // ever(
-    //   count,
-    //   (_) => print('Menjalankan Perintah ....'),
-    // );
+//     // TODO: Ever Berguna untuk melihat single data yg berjalan
+//     // ever(
+//     //   count,
+//     //   (_) => print('Menjalankan Perintah ....'),
+//     // );
 
-    // TODO: everALL Berguna untuk melihat multi data yg berjalan, dilihat dari penggunaan List pada syntaxnya
-    // everAll(
-    //   [count, data],
-    //   (_) => print('Menjalankan Perintah ....'),
-    // );
+//     // TODO: everALL Berguna untuk melihat multi data yg berjalan, dilihat dari penggunaan List pada syntaxnya
+//     // everAll(
+//     //   [count, data],
+//     //   (_) => print('Menjalankan Perintah ....'),
+//     // );
 
-    // TODO: once Berguna untuk melihat data yg berjalan hanya sekali
-    // once(
-    //   count,
-    //   (_) => print('Menjalankan Perintah ....'),
-    // );
+//     // TODO: once Berguna untuk melihat data yg berjalan hanya sekali
+//     // once(
+//     //   count,
+//     //   (_) => print('Menjalankan Perintah ....'),
+//     // );
 
-    // TODO: debounce Berguna untuk melihat data yg berjalan dan terdapat delay sesuai yg diperintahkan, dilihat terdapat parameter Time Duration
-    // debounce(
-    //   count,
-    //   (_) => print('Menjalankan Perintah ....'),
-    //   time: Duration(
-    //     seconds: 3,
-    //   ),
-    // );
+//     // TODO: debounce Berguna untuk melihat data yg berjalan dan terdapat delay sesuai yg diperintahkan, dilihat terdapat parameter Time Duration
+//     // debounce(
+//     //   count,
+//     //   (_) => print('Menjalankan Perintah ....'),
+//     //   time: Duration(
+//     //     seconds: 3,
+//     //   ),
+//     // );
 
-    // TODO: interval hampir mirip dengan debounce, yg jadi pembeda, jika ada proses pada perubahan data, data tetap dapat dibaca tetapi didelay sesuai perintah
-    // interval(
-    //   count,
-    //   (_) => print('Menjalankan Perintah ....'),
-    //   time: Duration(
-    //     seconds: 3,
-    //   ),
-    // );
+//     // TODO: interval hampir mirip dengan debounce, yg jadi pembeda, jika ada proses pada perubahan data, data tetap dapat dibaca tetapi didelay sesuai perintah
+//     // interval(
+//     //   count,
+//     //   (_) => print('Menjalankan Perintah ....'),
+//     //   time: Duration(
+//     //     seconds: 3,
+//     //   ),
+//     // );
 
-    super.onInit();
-  }
-}
+//     super.onInit();
+//   }
+// }
--- lib/getx5_workers/get5.dart
+++ lib/getx5_workers/get5.dart
@@ -1,50 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx_state_management/getx5_workers/controller.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:getx_state_management/getx5_workers/controller.dart';
 
-class Get5 extends StatelessWidget {
-  final myC = Get.put(Controller());
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Text Field'),
-        actions: [
-          IconButton(
-            onPressed: () => myC.reset(),
-            icon: Icon(
-              Icons.refresh,
-            ),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Obx(
-                () => Text(
-                  'Terjadi sesuatu: ${myC.count} x',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                onChanged: (value) => myC.change(),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class Get5 extends StatelessWidget {
+//   final myC = Get.put(Controller());
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Text Field'),
+//         actions: [
+//           IconButton(
+//             onPressed: () => myC.reset(),
+//             icon: Icon(
+//               Icons.refresh,
+//             ),
+//           ),
+//         ],
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Obx(
+//                 () => Text(
+//                   'Terjadi sesuatu: ${myC.count} x',
+//                   style: TextStyle(
+//                     fontSize: 30,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               TextField(
+//                 onChanged: (value) => myC.change(),
+//                 decoration: InputDecoration(
+//                   border: OutlineInputBorder(),
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
