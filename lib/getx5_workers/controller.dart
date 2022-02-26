556e032aad416c5ed6032776b4ccda4efdfa31f5
aprxty3 <ajiprxty3@gmail.com>
2022-02-27 03:41:53 +0700

    try oninit ever & everALL

--- lib/getx5_workers/controller.dart
+++ lib/getx5_workers/controller.dart
@@ -2,8 +2,28 @@ import 'package:get/get.dart';
 
 class Controller extends GetxController {
   var count = 0.obs;
+  var data = 0.obs;
 
   void change() => count++;
 
   void reset() => count.value = 0;
+
+  @override
+  void onInit() {
+    // Workers
+
+    // TODO: Ever Berguna untuk melihat single data yg berjalan
+    // ever(
+    //   count,
+    //   (_) => print('Menjalankan Perintah ....'),
+    // );
+
+    // TODO: everALL Berguna untuk melihat multi data yg berjalan, dilihat dari penggunaan List pada syntaxnya
+    // everAll(
+    //   [count, data],
+    //   (_) => print('Menjalankan Perintah ....'),
+    // );
+
+    super.onInit();
+  }
 }
