2ea20ec9b0ae6ea64743a88e44df96227a708e09
aprxty3 <ajiprxty3@gmail.com>
2022-02-27 03:58:15 +0700

    build page

---
+++ lib/getx6_navigation/get6.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Get6 extends StatelessWidget {
+  const Get6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Navigation',
+        ),
+      ),
+    );
+  }
+}
