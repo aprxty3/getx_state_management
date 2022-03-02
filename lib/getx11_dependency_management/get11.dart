1109efdd128746eb80823c34e3abd2be5ef499db
aprxty3 <ajiprxty3@gmail.com>
2022-03-03 00:03:47 +0700

    commented

--- lib/getx11_dependency_management/get11.dart
+++ lib/getx11_dependency_management/get11.dart
@@ -1,98 +1,98 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-class Get11 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () => Get.to(() => NextPage()),
-            icon: Icon(
-              Icons.ac_unit,
-            ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Home Page',
-          style: TextStyle(fontSize: 32),
-        ),
-      ),
-    );
-  }
-}
+// class Get11 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: [
+//           IconButton(
+//             onPressed: () => Get.to(() => NextPage()),
+//             icon: Icon(
+//               Icons.ac_unit,
+//             ),
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Home Page',
+//           style: TextStyle(fontSize: 32),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class NextPage extends StatelessWidget {
-  // final MyController c = Get.find();
-  final c = Get.put(
-    MyController(),
-    permanent: true,
-    tag: 'tag-count',
-  );
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () => Get.to(() => TextPage()),
-            icon: Icon(
-              Icons.ac_unit,
-            ),
-          ),
-        ],
-      ),
-      body: Obx(() => Center(
-            child: Text(
-              '${c.count}',
-              style: TextStyle(fontSize: 32),
-            ),
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => c.add(),
-        child: Icon(
-          Icons.add,
-        ),
-      ),
-    );
-  }
-}
+// class NextPage extends StatelessWidget {
+//   // final MyController c = Get.find();
+//   final c = Get.put(
+//     MyController(),
+//     permanent: true,
+//     tag: 'tag-count',
+//   );
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: [
+//           IconButton(
+//             onPressed: () => Get.to(() => TextPage()),
+//             icon: Icon(
+//               Icons.ac_unit,
+//             ),
+//           ),
+//         ],
+//       ),
+//       body: Obx(() => Center(
+//             child: Text(
+//               '${c.count}',
+//               style: TextStyle(fontSize: 32),
+//             ),
+//           )),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => c.add(),
+//         child: Icon(
+//           Icons.add,
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class TextPage extends StatelessWidget {
-  // final c = Get.put(
-  //   MyController(),
-  //   permanent: false,
-  //   tag: 'tag-text',
-  // );
+// class TextPage extends StatelessWidget {
+//   // final c = Get.put(
+//   //   MyController(),
+//   //   permanent: false,
+//   //   tag: 'tag-text',
+//   // );
 
-  final c = Get.find<MyController>(tag: 'tag-text');
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: TextField(
-            controller: c.textC,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   final c = Get.find<MyController>(tag: 'tag-text');
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20),
+//         child: Center(
+//           child: TextField(
+//             controller: c.textC,
+//             decoration: InputDecoration(
+//               border: OutlineInputBorder(),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class MyController extends GetxController {
-  var count = 0.obs;
-  var textC = TextEditingController();
+// class MyController extends GetxController {
+//   var count = 0.obs;
+//   var textC = TextEditingController();
 
-  void add() {
-    count++;
-  }
-}
+//   void add() {
+//     count++;
+//   }
+// }
