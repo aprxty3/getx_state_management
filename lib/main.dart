aaefb31a1e63299527e607d8f5fcfe8a1568cb8c
aprxty3 <ajiprxty3@gmail.com>
2022-03-03 00:03:42 +0700

    change navigator

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_state_management/getx10_bottom_sheet/get10.dart';
 import 'package:getx_state_management/getx11_dependency_management/get11.dart';
+import 'package:getx_state_management/getx12_getPutAsync/get12.dart';
 import 'package:getx_state_management/getx2_state_manager/getx2.dart';
 import 'package:getx_state_management/getx3_uniqueID/get3.dart';
 import 'package:getx_state_management/getx4_life_cycle/get4.dart';
@@ -20,17 +21,11 @@ import 'getx1_counter/getx1.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // final myC = Get.put(MyController());
-  final c = Get.lazyPut(
-    () => MyController(),
-    tag: 'tag-text',
-    fenix: true,
-  );
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.light(),
-      home: Get11(),
+      home: Get12(),
     );
   }
 }
