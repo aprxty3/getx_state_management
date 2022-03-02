86e623df4f0a254acfe16dff9bf2032bb89fb417
aprxty3 <ajiprxty3@gmail.com>
2022-03-02 20:15:54 +0700

    buildpage

---
+++ lib/getx9_dialog/get9.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Get9 extends StatelessWidget {
+  const Get9({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
