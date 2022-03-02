06dac37dce651441b4c792208c40b1e276c8283d
aprxty3 <ajiprxty3@gmail.com>
2022-03-02 21:13:25 +0700

    build page

---
+++ lib/getx10_bottom_sheet/get10.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Get10 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
