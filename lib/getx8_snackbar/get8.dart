8c2ecf5d82b1d35a6b5ad11061f0fc248e1f0b3c
aprxty3 <ajiprxty3@gmail.com>
2022-03-02 20:13:39 +0700

    try to implement some parameters

--- lib/getx8_snackbar/get8.dart
+++ lib/getx8_snackbar/get8.dart
@@ -14,7 +14,26 @@ class Get8 extends StatelessWidget {
           //   ),
           // );
 
-          Get.snackbar('Ini Judul', 'Ini Isi');
+          Get.snackbar(
+            'Ini Judul',
+            'Ini Isi',
+            animationDuration: Duration(milliseconds: 100),
+            backgroundColor: Colors.red,
+            backgroundGradient: LinearGradient(
+              colors: [
+                Colors.green,
+                Colors.purple,
+              ],
+            ),
+            borderWidth: 2,
+            borderColor: Colors.red,
+            // snackPosition: SnackPosition.BOTTOM,
+            // snackStyle: SnackStyle.GROUNDED,
+            onTap: (_) {
+              print('Ini Di Klik');
+            },
+            icon: Icon(Icons.ac_unit),
+          );
         },
         child: Icon(
           Icons.add,
