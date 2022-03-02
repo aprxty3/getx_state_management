bfca58de26677bd76197b73f0019f36f0cc3b8f3
aprxty3 <ajiprxty3@gmail.com>
2022-03-02 23:37:29 +0700

    try to learn textfield with permanent

--- lib/getx11_dependency_management/get11.dart
+++ lib/getx11_dependency_management/get11.dart
@@ -31,7 +31,16 @@ class NextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Get.to(() => TextPage()),
+            icon: Icon(
+              Icons.ac_unit,
+            ),
+          ),
+        ],
+      ),
       body: Obx(() => Center(
             child: Text(
               '${c.count}',
@@ -48,8 +57,30 @@ class NextPage extends StatelessWidget {
   }
 }
 
+class TextPage extends StatelessWidget {
+  final c = Get.find<MyController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: TextField(
+            controller: c.textC,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class MyController extends GetxController {
   var count = 0.obs;
+  var textC = TextEditingController();
 
   void add() {
     count++;
