b12f29dfce8ec06d0f5c486e707b8ef0c8b17a74
aprxty3 <ajiprxty3@gmail.com>
2022-03-02 23:19:17 +0700

    try to create count controller

--- lib/getx11_dependency_management/get11.dart
+++ lib/getx11_dependency_management/get11.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Get11 extends StatelessWidget {
+  final MyController c = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Obx(() => Center(
+            child: Text(
+              '${c.count}',
+              style: TextStyle(fontSize: 32),
+            ),
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => c.add(),
+        child: Icon(
+          Icons.add,
+        ),
+      ),
     );
+  }
+}
+
+class MyController extends GetxController {
+  var count = 0.obs;
+
+  void add() {
+    count++;
   }
 }
