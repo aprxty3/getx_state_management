e915c3cefaf6fb42cc4fe6eb5229a931ffc593b2
aprxty3 <ajiprxty3@gmail.com>
2022-03-08 22:59:26 +0700

    build page

---
+++ lib/getx13_getCreate/get13.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Get13 extends StatelessWidget {
+  const Get13({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
