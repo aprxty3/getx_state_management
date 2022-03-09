58ac86f0e5e24047f6e9f00f464b5b91368061cc
aprxty3 <ajiprxty3@gmail.com>
2022-03-10 01:05:09 +0700

    get.putAsync done

---
+++ lib/getx12_getPutAsync/controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Controller extends GetxController {
+  var count = 0.obs;
+
+  Future<void> setData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('counter', 99);
+
+    count.value = prefs.getInt('counter')!.toInt();
+  }
+}
