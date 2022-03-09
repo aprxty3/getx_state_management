2c177ab961419923da3de4ff0b9af46981d2bdf0
aprxty3 <ajiprxty3@gmail.com>
2022-03-10 03:31:19 +0700

    onboard

--- lib/getx14_bindings/get14.dart
+++ lib/getx14_bindings/get14.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_state_management/getx14_bindings/count_page.dart';
 
 class Get14 extends StatelessWidget {
   const Get14({Key? key}) : super(key: key);
@@ -6,8 +8,26 @@ class Get14 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Homepage',
+              style: TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(
+              onPressed: () => Get.to(
+                CountPage(),
+              ),
+              child: Text('To The Moon'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
