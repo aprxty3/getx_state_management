22facd7f4cd72a66b156eac6b01dfde81b67835a
aprxty3 <ajiprxty3@gmail.com>
2022-03-09 23:48:31 +0700

    build get bindings

---
+++ lib/getx14_bindings/get14.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Get14 extends StatelessWidget {
+  const Get14({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
