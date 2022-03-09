cb698d9028b9db829f918d1f48bbfc4685dab5f4
aprxty3 <ajiprxty3@gmail.com>
2022-03-10 03:54:02 +0700

    commented

--- lib/getx6_navigation/get6_1.dart
+++ lib/getx6_navigation/get6_1.dart
@@ -1,34 +1,34 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx_state_management/getx6_navigation/routes/route_name.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:getx_state_management/getx6_navigation/routes/route_name.dart';
 
-class Get6_1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Page 1',
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.toNamed(RouteName.page_2);
-              },
-              child: Text(
-                'Next Page',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class Get6_1 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Page 1',
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 Get.toNamed(RouteName.page_2);
+//               },
+//               child: Text(
+//                 'Next Page',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/getx6_navigation/get6_2.dart
+++ lib/getx6_navigation/get6_2.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx_state_management/getx6_navigation/get6_3.dart';
-import 'package:getx_state_management/getx6_navigation/routes/route_name.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:getx_state_management/getx6_navigation/get6_3.dart';
+// import 'package:getx_state_management/getx6_navigation/routes/route_name.dart';
 
-class Get6_2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Page 2',
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.toNamed(RouteName.page_3);
-              },
-              child: Text(
-                'Next Page',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Get.back();
-              },
-              child: Text(
-                'Back Page',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class Get6_2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Page 2',
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 Get.toNamed(RouteName.page_3);
+//               },
+//               child: Text(
+//                 'Next Page',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(
+//               height: 30,
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 Get.back();
+//               },
+//               child: Text(
+//                 'Back Page',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
