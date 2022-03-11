e2875bf06d978ac6e808eced0444639dcf5a4681
aprxty3 <ajiprxty3@gmail.com>
2022-03-11 09:24:50 +0700

    add navigator

--- lib/getx15_translations/get15.dart
+++ lib/getx15_translations/get15.dart
@@ -6,7 +6,29 @@ class Get15 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Homepage',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click Me'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
