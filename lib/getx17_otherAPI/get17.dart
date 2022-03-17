a934a4a1ac700fc81e7bbb9927f5c23bf9ba09f3
aprxty3 <ajiprxty3@gmail.com>
2022-03-17 21:46:15 +0700

    create argument

--- lib/getx17_otherAPI/get17.dart
+++ lib/getx17_otherAPI/get17.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_state_management/getx17_otherAPI/get17_2.dart';
 
 class Get17 extends StatelessWidget {
   const Get17({Key? key}) : super(key: key);
@@ -7,11 +8,25 @@ class Get17 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Homepage')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            Text(
+              'Hai Ini Homepage',
+              style: TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(
+                onPressed: () => Get.to(
+                      () => Get17_2(),
+                      arguments: 'Hay From first page',
+                    ),
+                child: Text(
+                  'Next Page',
+                  style: TextStyle(fontSize: 25),
+                ))
+          ],
         ),
       ),
     );
