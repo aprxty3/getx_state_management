5281654b338a18e5b941d045bb2cfc0658e5b17d
aprxty3 <ajiprxty3@gmail.com>
2022-03-17 21:33:03 +0700

    build page

---
+++ lib/getx17_otherAPI/get17.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Get17 extends StatelessWidget {
+  const Get17({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
