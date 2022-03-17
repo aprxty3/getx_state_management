29d868d3ded059b045ba9421db6e690eab1750d8
aprxty3 <ajiprxty3@gmail.com>
2022-03-17 22:22:53 +0700

    add some paramater

--- lib/getx17_otherAPI/get17_2.dart
+++ lib/getx17_otherAPI/get17_2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_state_management/getx17_otherAPI/get17_3.dart';
 
 class Get17_2 extends StatelessWidget {
   const Get17_2({Key? key}) : super(key: key);
@@ -12,14 +13,22 @@ class Get17_2 extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(
               "${Get.arguments}",
               style: TextStyle(fontSize: 25),
             ),
+            Text(
+              "${Get.rawRoute}",
+              style: TextStyle(fontSize: 25),
+            ),
+            Text(
+              "${Get.previousRoute}",
+              style: TextStyle(fontSize: 25),
+            ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => Get.to(() => Get17_3()),
               child: Text(
                 'Next Page',
                 style: TextStyle(fontSize: 25),
