30f489029d3e8df2efbd1117c8cd0e4166a0e10b
aprxty3 <ajiprxty3@gmail.com>
2022-03-18 10:52:54 +0700

    homepage

---
+++ lib/getx18_stateMixin/get18.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Get18 extends StatelessWidget {
+  const Get18({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ini Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
