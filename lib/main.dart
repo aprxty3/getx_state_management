e6f683dec5efafa7ffdc12dd73eeeef3e9ce744d
aprxty3 <ajiprxty3@gmail.com>
2022-03-18 11:30:26 +0700

    implement bindings & delet get.put

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_state_management/getx17_otherAPI/get17.dart';
+import 'package:getx_state_management/getx18_stateMixin/bindings.dart';
 import 'package:getx_state_management/getx18_stateMixin/get18.dart';
 
 import 'getx18_stateMixin/controlle.dart';
@@ -8,11 +9,11 @@ import 'getx18_stateMixin/controlle.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final myC = Get.put(MyC());
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.light(),
+      initialBinding: MyBinding(),
       home: Get18(),
     );
   }
