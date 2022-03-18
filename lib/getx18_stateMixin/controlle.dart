9ce24ef15465161f0665eec2c4e1d37951fa4b3a
aprxty3 <ajiprxty3@gmail.com>
2022-03-18 14:44:23 +0700

    get api from reqres

--- lib/getx18_stateMixin/controlle.dart
+++ lib/getx18_stateMixin/controlle.dart
@@ -1,7 +1,36 @@
 import 'package:get/get.dart';
+import './provider.dart';
 
-class MyC extends GetxController {
-  var count = 0.obs;
+class MyC extends GetxController with StateMixin<Map<String, dynamic>> {
+  void getData() {
+    try {
+      UserProvider().getUser().then((response) {
+        if (response.statusCode == 200) {
+          final data = response.body["data"] as Map<String, dynamic>;
+          change(
+            data,
+            status: RxStatus.success(),
+          );
+        }
+      }, onError: (_) {
+        change(
+          null,
+          status: RxStatus.error('Tidak dapat data dari API'),
+        );
+      });
+    } catch (err) {
+      change(
+        null,
+        status: RxStatus.error(
+          err.toString(),
+        ),
+      );
+    }
+  }
 
-  void add() => count++;
+  @override
+  void onInit() {
+    change(null, status: RxStatus.empty());
+    super.onInit();
+  }
 }
