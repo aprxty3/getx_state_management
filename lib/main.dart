5aab4aed85d95eac1c1a94bb922984dba80b0ef7
aprxty3 <ajiprxty3@gmail.com>
2022-03-18 20:50:23 +0700

    implement approute

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_state_management/getx19_getUtils/get19.dart';
+import 'package:getx_state_management/getx19_getStorage/get19_0.dart';
+import 'package:getx_state_management/getx19_getStorage/get19_1.dart';
+import 'package:getx_state_management/getx19_getStorage/route/app_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.light(),
-      home: Get19(),
+      home: Get19_1(),
+      getPages: AppPage.pages,
     );
   }
 }
