ab7710ebb07813380e8a3ef4bdf6a3e87b6c35b5
aprxty3 <ajiprxty3@gmail.com>
2022-03-19 14:45:30 +0700

    implement log out function

--- lib/getx19_getStorage/get19_0.dart
+++ lib/getx19_getStorage/get19_0.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_state_management/getx19_getStorage/%20controller/auth.dart';
+import 'package:getx_state_management/getx19_getStorage/%20controller/login_controller.dart';
 
 class Get19_0 extends StatelessWidget {
-  const Get19_0({Key? key}) : super(key: key);
+  final auth = Get.find<AuthC>();
+  final login = Get.find<LoginC>();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,7 @@ class Get19_0 extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => auth.logOut(login.rememberMe.value),
         child: Icon(Icons.logout),
       ),
     );
